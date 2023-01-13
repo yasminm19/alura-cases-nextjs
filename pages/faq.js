8c781278cda24aa037f7b8a25eb30a3f3b68b129
yasminm19 <yasminmichaella19@gmail.com>
2023-01-13 11:43:09 -0300

    add-faq-page

--- pages/faq.js
+++ pages/faq.js
@@ -1,5 +1,10 @@
-import Link from '../src/components/Link'
-import Head from 'next/head';
+// import Link from '../src/components/Link'
+// import Head from 'next/head';
+import FAQScreen from "../src/screens/FAQScreen";
+
+export default FAQScreen;
+
+
 export async function getStaticProps() {
   const FAQ_API_URL =
     'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json'
@@ -18,27 +23,3 @@ export async function getStaticProps() {
   }
 }
 
-export default function FAQPage({ faq }) {
-  return (
-    <div>
-      <Head>
-        <title>FAQ - Alura Cases</title>
-      </Head>
-
-      <h1>Alura Cases - Páginas de perguntas FAQ</h1>
-      <Link href="/">
-        Ir para a Home
-      </Link>
-      <ul>
-        {faq.map(({ answer, question }) => (
-          <li>
-            <article>
-              <h2> {question} </h2>
-              <p> {answer} </p>
-            </article>
-          </li>
-        ))}
-      </ul>
-    </div>
-  )
-}
