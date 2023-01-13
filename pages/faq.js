45b3c0a0f1f512571c53a0a46ec5779334ac795e
yasminm19 <yasminmichaella19@gmail.com>
2023-01-13 11:29:51 -0300

    :tada: first commit

---
+++ pages/faq.js
@@ -0,0 +1,44 @@
+import Link from '../src/components/Link'
+import Head from 'next/head';
+export async function getStaticProps() {
+  const FAQ_API_URL =
+    'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json'
+  const faq = await fetch(FAQ_API_URL)
+    .then(respostaDoServidor => {
+      return respostaDoServidor.json()
+    })
+    .then(resposta => {
+      return resposta
+    })
+
+  return {
+    props: {
+      faq
+    }
+  }
+}
+
+export default function FAQPage({ faq }) {
+  return (
+    <div>
+      <Head>
+        <title>FAQ - Alura Cases</title>
+      </Head>
+
+      <h1>Alura Cases - Páginas de perguntas FAQ</h1>
+      <Link href="/">
+        Ir para a Home
+      </Link>
+      <ul>
+        {faq.map(({ answer, question }) => (
+          <li>
+            <article>
+              <h2> {question} </h2>
+              <p> {answer} </p>
+            </article>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
---
+++ pages/index.js
@@ -0,0 +1,46 @@
+import HomeScreen from  "../src/screens/HomeScreen";
+
+export default HomeScreen;
+
+
+// import React from 'react';
+// import Link from '../src/components/Link';
+// import Footer from '../src/components/patterns/Footer';
+// import HomeScreen from '../src/screens/HomeScreen';
+// import GlobalStyle from '../src/theme/GlobalStyle';
+
+
+
+// function Title({ children, as }) {
+//   const Tag = as;
+//   return (
+//     <React.Fragment>
+//           <h1>
+//               {children}
+//           </h1>
+//           <style jsx>{`
+//             ${Tag} {
+//               color: red;
+              
+//             }
+//           `}</style>
+//     </React.Fragment>
+  
+//   );
+// }
+
+// export default function HomePage() {
+//   return (
+//     <div>
+      
+//       <Title as="h1">Alura Cases - Home Page</Title>
+//           <Link href="/faq">
+//           Ir para a página do FAQ
+//           </Link>
+//           <HomeScreen />
+//           <GlobalStyle />
+         
+         
+//     </div>
+//   )
+// }
